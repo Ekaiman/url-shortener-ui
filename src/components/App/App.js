e967df80b0589878a8e598ddfbec6c09f8888f64
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-04-26 10:10:21 -0600

    Move fetch and create post

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,48 @@
-import React, { Component } from 'react';
-import './App.css';
-import { getUrls } from '../../apiCalls';
-import UrlContainer from '../UrlContainer/UrlContainer';
-import UrlForm from '../UrlForm/UrlForm';
+import React, { Component } from 'react'
+import './App.css'
+import UrlContainer from '../UrlContainer/UrlContainer'
+import UrlForm from '../UrlForm/UrlForm'
+import getUrls from '../../apiCalls.js'
 
 export class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       urls: []
     }
   }
 
-  componentDidMount() {
+  componentDidMount = () => {
+    fetch('http://localhost:3001/api/v1/urls')
+      .then(response => response.json())
+      .then(data => this.setState({ urls: [...data.urls] }))
+  }
+
+  createShortUrl = urlObj => {
+    fetch('http://localhost:3001/api/v1/urls', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        long_url: urlObj.urlToShorten,
+        title: urlObj.title
+      })
+    })
+      .then(response => response.json())
+      .then(data => this.setState({ urls: [...this.state.urls, data] }))
   }
 
   render() {
     return (
-      <main className="App">
+      <main className='App'>
         <header>
           <h1>URL Shortener</h1>
-          <UrlForm />
+          <UrlForm createShortUrl={this.createShortUrl} />
         </header>
 
-        <UrlContainer urls={this.state.urls}/>
+        <UrlContainer urls={this.state.urls} />
       </main>
-    );
+    )
   }
 }
 
-export default App;
+export default App
